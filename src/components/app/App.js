0c6eee29e83676d351c4f27cece51b085b1827ce
roman <roman@example.ee>
2021-09-28 10:40:46 +0300

    React project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -89,7 +89,7 @@ export default class App extends Component {
     }
 
     return items.filter((item) => {
-      return item.label.indexOf(term) > 1
+      return item.label.indexOf(term) > -1
     });
   }
 
